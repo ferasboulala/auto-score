8ff8acf8f24f791bd6d7ebfb285fed36c0e5a818
feras <ferasboulala@gmail.com>
2018-12-26 00:59:32 -0500

    Got 0.1 avg log-loss with 3 dissimilar glyphs. Will have to rework on data pipeline for dataset balancing.

--- src/autoscore/model.py
+++ src/autoscore/model.py
@@ -22,7 +22,7 @@ DEF_TRANSFORM = transforms.Compose([transforms.Resize((DEF_H, DEF_W), interpolat
 
 
 class DatasetLoader(Dataset):
-    def __init__(self, dir_fn=DEF_DIR_FN, transform=DEF_TRANSFORM):
+    def __init__(self, dir_fn=DEF_DIR_FN, transform=DEF_TRANSFORM, train=True):
         super(DatasetLoader, self).__init__()
         self.transform = transform
         self.X_fn = []
@@ -34,6 +34,8 @@ class DatasetLoader(Dataset):
                     x = join(prefix, file), self.lookup[name]
                     self.X_fn.append(x)
         shuffle(self.X_fn)
+        if not train:
+            self.X_fn = self.X_fn[0:1000]
 
     def __len__(self):
         return len(self.X_fn)
@@ -47,7 +49,7 @@ class DatasetLoader(Dataset):
 
 
 class Net(nn.Module):
-    def __init__(self, n_classes, n_conv=2, n_maps=15):
+    def __init__(self, n_classes, n_conv=2, n_maps=30):
         super(Net, self).__init__()
         self.n_maps = n_maps
         self.conv = []
@@ -57,19 +59,19 @@ class Net(nn.Module):
                 continue
             self.conv.append(nn.Conv2d(i * n_maps, (i+1) * n_maps, kernel_size=3))
         self.fc_size = self._get_conv_size(DEF_H, DEF_W)
-        self.fc1 = nn.Linear(self.fc_size, 100)
-        self.fc2 = nn.Linear(100, n_classes)
-        # self.fc3 = nn.Linear(100, n_classes)
+        self.fc1 = nn.Linear(self.fc_size, 200)
+        self.fc2 = nn.Linear(200, 100)
+        self.fc3 = nn.Linear(100, n_classes)
 
     def forward(self, x):
         for conv in self.conv:
             x = F.max_pool2d(F.relu(conv(x)), 2)
         x = x.view(-1, self.fc_size)
         x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
+        # x = F.dropout(x, training=self.training)1
         x = F.relu(self.fc2(x))
         # x = F.dropout(x, training=self.training)
-        # x = F.relu(self.fc3(x))
+        x = self.fc3(x)
         return F.log_softmax(x, dim=1)
 
     @staticmethod
@@ -102,7 +104,7 @@ def test(args, model, test_loader):
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in test_loader[0:1000]:
+        for data, target in test_loader:
             output = model(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
             pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
@@ -120,7 +122,7 @@ def main():
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+    parser.add_argument('--epochs', type=int, default=20, metavar='N',
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                         help='learning rate (default: 0.01)')
@@ -138,13 +140,14 @@ def main():
 
     my_loader = DatasetLoader()
     train_loader = DataLoader(my_loader, batch_size=10, shuffle=True)
+    # test_loader = DataLoader(DatasetLoader(train=False))
 
     model = Net(n_classes=len(my_loader.lookup))
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
     for epoch in range(1, args.epochs + 1):
         train(args, model, train_loader, optimizer, epoch)
-        # test(args, model, train_loader)
+        # test(args, model, test_loader)
 
 
 if __name__ == '__main__':
